c7536e781f13da8b93ca6bce52dfde7901101976
Daniel Galvão <galvaodev@gmail.com>
2017-07-25 20:27:56 -0300

    Using components and shared components

---
+++ frontend/src/components/shared/Foto/Foto.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="painel">
+    <h2 class="painel-titulo">{{ titulo }}</h2>
+    <slot class="painel-conteudo">
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['titulo']
+}
+</script>
+
+<style lang="css">
+</style>

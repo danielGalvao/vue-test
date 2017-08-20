0bad5f9db2e984300f035f187101cd130e92a5be
Daniel Galvão <galvaodev@gmail.com>
2017-08-20 16:22:28 -0300

    Using directives exporting default and directive parameters

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -22,12 +22,15 @@
 const urlFotos = 'http://localhost:3000/v1/fotos'
 import Foto from '../shared/foto/Foto.vue'
 import Btn from '../shared/button/Button.vue'
-
+import transform from '../../directives/Transform'
 export default {
 
   components: {
     foto: Foto,
     btn: Btn
+  },
+  directives: {
+    'my-transform': transform
   },
   methods: {
     remove(foto){
--- frontend/src/directives/Transform.js
+++ frontend/src/directives/Transform.js
@@ -1,19 +1,17 @@
-import Vue from 'vue'
-
-Vue.directive('my-transform', {
+export default {
   bind(el, binding, vnode) {
     let current = 0
-    
+
     el.addEventListener('click', function(){
-      
+
       let increment = binding.value || 90
-      
+
       current+= increment
       el.style.transform = `rotate(${current}deg)`
-      
+
       if(binding.modifiers.animate){
         el.style.transition = 'transform 0.5s'
       }
     })
   }
-})
+}
